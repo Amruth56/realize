d1d5675ec1f5d5060c673f2e7675c1495ce1addb
Gusto Muffin <amruth.mandappa56@gmail.com>
2025-03-31 16:55:28 +0530

    created a page for all the links

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -13,12 +13,16 @@ const Header = () => {
         />
       </div>
       <div className="flex gap-6 items-center max-sm:hidden mr-10">
+      <a href="/listofpages" className="text-base text-gray-600 cursor-pointer">
+            List of Pages
+          </a>
           <a href="#" className="text-base text-gray-600 cursor-pointer">
             Documentation
           </a>
           <a href="#" className="text-base text-gray-600 cursor-pointer">
             Support
           </a>
+          
           <div className="flex gap-2 items-center cursor-pointer">
             <img
               src="https://cdn.builder.io/api/v1/image/assets/TEMP/b35e72e25cbd9d23a2ce400aa1c9b3fb4f1a9023"
